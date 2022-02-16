7041eadbde66a1fec7468d3091ee905bf789c4af
_Alz <alessio_fimognari@nest.co.uk>
2022-02-16 12:30:03 +0000

    fix(ui-components, eslint): removed a library from ui-components that forces a lower eslint version

--- lint-staged.config.mjs
+++ lint-staged.config.mjs
@@ -1,13 +1,10 @@
 export default {
-  'apps/AppShell/**/*.*': () => [
-    'npm run app-shell:eslint',
-    'npm run app-shell:test',
-    'npm run app-shell:ratchet',
-  ],
-  'apps/MFE/Header/**/*.*': () => [
-    'npm run header:eslint',
-    'npm run header:test',
-    'npm run header:ratchet',
-  ],
+  'apps/AppShell/**/*.*': () => ['npm run app-shell:eslint', 'npm run app-shell:test', 'npm run app-shell:ratchet'],
+  'apps/MFE/Header/**/*.*': () => ['npm run header:eslint', 'npm run header:test', 'npm run header:ratchet'],
   'libs/federated-modules/**/*.*': () => ['npm run federated-modules:eslint'],
+  'libs/ui-components/**/*.*': () => [
+    'npm run ui-components:lint',
+    'npm run ui-components:test',
+    'npm run ui-components:ratchet',
+  ],
 }
