8eaab854a6944ac7ad2a612673edb285181a3b44
Greg Toth <greg.toth@amido.com>
2022-02-24 10:16:32 +0000

    style(app-shell/server): fixing linting

--- apps/AppShell/src/server/index.ts
+++ apps/AppShell/src/server/index.ts
@@ -1,10 +1,9 @@
 import 'node-self'
-import path from "path"
+import path from 'path'
 
 // eslint-disable-next-line import/no-extraneous-dependencies
 import {htmlMiddleware} from '@next/middlewares'
 import express from 'express'
-
 
 import {renderMiddleware} from './middleware/render'
 
