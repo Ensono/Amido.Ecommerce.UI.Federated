fdf53e218248d4693fecf92cea42e267b37bb888
Levon Barsegjan <levon.barsegjan@amido.com>
2022-02-28 11:14:24 +0000

    feat(appshell): implemented error handling for federateComponent

--- libs/framework/federate-component/src/index.tsx
+++ libs/framework/federate-component/src/index.tsx
@@ -1,11 +1,47 @@
+import React, {Suspense} from 'react'
+
+import Logger from '@next/core-logger'
+
 import {getClientComponent} from './client'
 import {getServerComponent} from './server'
 import {RemotesContext} from './types'
 
 export const context: RemotesContext = {}
 
+class ErrorBoundary extends React.Component<any, {hasError: boolean}> {
+  constructor(props: any) {
+    super(props)
+    this.state = {hasError: false}
+  }
+
+  static getDerivedStateFromError() {
+    return {hasError: true}
+  }
+
+  componentDidCatch(error: Error) {
+    Logger.error(error)
+  }
+
+  render() {
+    if (this.state.hasError) {
+      if (this.props.fallback) {
+        return this.props.fallback
+      }
+
+      return null
+    }
+
+    return this.props.children
+  }
+}
+
 export const federateComponent = (remote: string, module: string, remoteUrl: string, shareScope = 'default') => {
-  const FederatedComponent: React.FC = ({children, ...props}) => {
+  const FederatedComponent: React.FC<{loadingFallback?: JSX.Element; errorFallback?: JSX.Element}> = ({
+    children,
+    loadingFallback = <> </>,
+    errorFallback = <> </>,
+    ...props
+  }) => {
     let Component: React.FC
 
     if (typeof window !== 'undefined') {
@@ -16,7 +52,13 @@ export const federateComponent = (remote: string, module: string, remoteUrl: str
       return null
     }
 
-    return <Component {...props}>{children}</Component>
+    return (
+      <Suspense fallback={loadingFallback}>
+        <ErrorBoundary fallback={errorFallback}>
+          <Component {...props}>{children}</Component>
+        </ErrorBoundary>
+      </Suspense>
+    )
   }
 
   return FederatedComponent
