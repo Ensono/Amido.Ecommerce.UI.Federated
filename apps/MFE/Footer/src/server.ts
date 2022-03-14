49af874807f4be3655da264e1e6ea4300668fba7
Alessio Fimognari <alessiofimognari@AML0401.local>
2022-03-14 09:40:51 +0000

    feat(framework): pORT from env

--- apps/MFE/Footer/src/server.ts
+++ apps/MFE/Footer/src/server.ts
@@ -1,6 +1,8 @@
+import {env} from 'process'
+
 import {Logger} from '@next/core-logger'
 
 import server from './server/'
 
-const PORT = process.env.PORT || '3003'
+const PORT = env.PORT
 server.listen(PORT, () => Logger.info(`webpack host: started at http://localhost:${PORT}`))
