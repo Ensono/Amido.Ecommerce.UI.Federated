a78b32d8a6ab1011d8a69e54ef496f029252cc8c
Robert Cass <contact@robertcass.dev>
2022-04-05 10:42:46 +0100

    chore(lint-staged): add text-positioner to lint-staged

--- lint-staged.config.mjs
+++ lint-staged.config.mjs
@@ -2,6 +2,11 @@ export default {
   'apps/AppShell/**/*.*': () => ['npm run app-shell:lint', 'npm run app-shell:test', 'npm run app-shell:ratchet'],
   'apps/MFE/Header/**/*.*': () => ['npm run header:lint', 'npm run header:test', 'npm run header:ratchet'],
   'apps/MFE/Footer/**/*.*': () => ['npm run footer:lint', 'npm run footer:test', 'npm run footer:ratchet'],
+  'apps/MFE/TextPositioner/**/*.*': () => [
+    'npm run text-positioner:lint',
+    'npm run text-positioner:test',
+    'npm run text-positioner:ratchet',
+  ],
   'libs/framework/federate-component/**/*.*': () => [
     'npm run federate-component:lint',
     'npm run federate-component:test',
