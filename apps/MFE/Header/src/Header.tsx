8ef44dd8ce0391e191ff8dfd39625a3d27472245
Greg Toth <greg.toth@amido.com>
2022-04-12 09:55:00 +0100

    feat(header): add custom hook to header

--- apps/MFE/Header/src/Header.tsx
+++ apps/MFE/Header/src/Header.tsx
@@ -10,8 +10,38 @@ const REMOTES = getRemoteUrls()
 
 const TextPositioner = federateComponent('mfe_text_positioner', './text-positioner', REMOTES.mfe_text_positioner)
 
+/**
+ * Example custom hook.
+ * @returns boolean
+ */
+const usePageBottom = () => {
+  const [bottom, setBottom] = useState(false)
+
+  useEffect(() => {
+    function handleScroll() {
+      const isBottom = window.innerHeight + document.documentElement.scrollTop === document.documentElement.offsetHeight
+      setBottom(isBottom)
+    }
+    window.addEventListener('scroll', handleScroll)
+    return () => {
+      window.removeEventListener('scroll', handleScroll)
+    }
+  }, [])
+
+  return bottom
+}
+
 const Header: React.FC = ({children}) => {
   const themeState = useThemeState()
+  const [counter, setCounter] = useState(0)
+  const isBottom = usePageBottom()
+
+  /*
+   * Test use effect works on component mount.
+   */
+  useEffect(() => {
+    console.log('Header useEffect onComponentDidMount')
+  }, [])
 
   /*
    * Test React Context. The Provider is coming from AppShell when federated.
@@ -20,10 +50,21 @@ const Header: React.FC = ({children}) => {
     console.log('ThemeState from Header', themeState)
   }, [themeState])
 
+  /*
+   * Test custom hook.
+   */
+  useEffect(() => {
+    console.log('Custom Hook, page scrolled down to bottom', isBottom)
+  }, [isBottom])
+
   return (
     <div>
-      <div onClick={() => alert('ciao mamma header')}>
+      <div>
         <p data-testid="paragraph-in-header">I am the HEADERRRRR'"</p>
+        <div>
+          <p>Counter: {counter}</p>
+          <button onClick={() => setCounter(counter + 1)}>Increment</button>
+        </div>
       </div>
       <TextPositioner
         loadingFallback={<div>loading text positioner...</div>}
