c9d9496c0e1131974ec6bf384c9b5c5ed98a2e83
Kyle Macquarrie <kyle_macquarrie@next.co.uk>
2022-04-27 17:36:34 +0100

    test(header): fix broken tests and add new ones

--- apps/MFE/Header/src/Header.test.tsx
+++ apps/MFE/Header/src/Header.test.tsx
@@ -1,10 +1,17 @@
+import {Provider} from 'react-redux'
+
 import {render, waitFor} from '@testing-library/react'
 
 import Header from './Header'
+import {headerStore} from './store'
 
 describe('Header', () => {
   it('renders as expected', async () => {
-    const {container} = render(<Header>foo bar</Header>)
+    const {container} = render(
+      <Provider store={headerStore}>
+        <Header>foo bar</Header>
+      </Provider>,
+    )
     await waitFor(() => {
       expect(container).toMatchSnapshot()
     })
