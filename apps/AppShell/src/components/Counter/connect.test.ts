2f1055b869db7881ea4b66a1326b510ad0a50d44
Kyle Macquarrie <kyle_macquarrie@next.co.uk>
2022-04-27 12:25:17 +0100

    refactor(app-shell): use connect pattern for app-shell and sketch out tests
    also fixes app tests by wrapping in a redux provider

---
+++ apps/AppShell/src/components/Counter/connect.test.ts
@@ -0,0 +1,13 @@
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+import {mapStateToProps, mergeProps} from './connect'
+
+describe('connect', () => {
+  describe('mapStateToProps', () => {
+    it.todo('returns the counter value')
+  })
+
+  describe('mergeProps', () => {
+    it.todo('creates an increment function')
+    it.todo('creates a reset function')
+  })
+})
---
+++ apps/AppShell/src/ducks/counter/index.test.ts
@@ -0,0 +1,7 @@
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+import {increment, set} from '.'
+
+describe('counter duck', () => {
+  it.todo('increments')
+  it.todo('resets')
+})
