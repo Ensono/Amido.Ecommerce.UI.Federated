460655116d515dfacdf9089967cd03ed8c1e8866
Kyle Macquarrie <kyle_macquarrie@next.co.uk>
2022-04-29 15:45:53 +0100

    fix(header): tidy up header scripts

--- apps/MFE/Header/src/Header.tsx
+++ apps/MFE/Header/src/Header.tsx
@@ -1,15 +1,8 @@
 import React from 'react'
 
-// import {federateComponent} from '@batman/federate-component'
-// import {getRemoteUrls} from '@batman/remote-urls'
-
 import HeaderCounter from './components/headerCounter'
 
-// const REMOTES = getRemoteUrls()
-
-// const TextPositioner = federateComponent('mfe_text_positioner', './text-positioner', REMOTES.mfe_text_positioner)
-
-const Header: React.FC = ({children}) => {
+const Header: React.FC = () => {
   return (
     <div>
       <HeaderCounter />
