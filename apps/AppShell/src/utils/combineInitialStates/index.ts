59dc2baf54e0d30463491634a6e12de0d3012f8a
Kyle Macquarrie <kyle_macquarrie@next.co.uk>
2022-04-29 09:23:06 +0100

    fix(app-shell): remove initial state elements from dom to avoid hydration issues

--- apps/AppShell/src/utils/combineInitialStates/index.ts
+++ apps/AppShell/src/utils/combineInitialStates/index.ts
@@ -7,8 +7,9 @@ export const combineInitialState = (initalState: any, state: any) => {
   Array.from(state).forEach((s: any) => {
     if (s.dataset.state) {
       const obj = JSON.parse(s.innerText.replaceAll('&quot;', '"'))
-      const keys = Object.keys(obj)
-      allstates[keys[0]] = obj[keys[0]]
+      const localKeys = Object.keys(obj)
+      allstates[localKeys[0]] = obj[localKeys[0]]
+      s.remove()
     }
   })
 
