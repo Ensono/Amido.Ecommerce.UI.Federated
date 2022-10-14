ee56adfe84b07f17c98256919f456bba48f3c5a1
theseanco <theseancotterill@live.com>
2022-10-14 16:17:10 +0100

    chore(tests): removed comment

--- libs/framework/middlewares/src/prerender.test.ts
+++ libs/framework/middlewares/src/prerender.test.ts
@@ -190,7 +190,6 @@ describe('prerender middleware', () => {
       expect(mockWrite).toHaveBeenNthCalledWith(1, `{"chunks":["http://localhost:3003/remote-entry.js"],"html":"`)
       expect(mockWrite).toHaveBeenNthCalledWith(2, testHtml)
       expect(mockWrite).toHaveBeenNthCalledWith(3, `","state":"NO STATE"}`)
-      //      expect(mockPipe).toHaveBeenCalled()
     })
   })
 })
